8ebeb809e384a39e3f9c5138da94be4e04f9c6b3
Philip Crockett <contact@philcrockett.com>
2024-08-04 13:12:33 +0200

    add local support to check subcommand

--- src/check_command.sh
+++ src/check_command.sh
@@ -3,4 +3,10 @@
 
 read_config
 configure_restic "${args[--dest]}"
+
+if [ "${args[--dest]}" == "local" ]; then
+    mount_device_by_uuid "${LOCAL_FILESYSTEM_UUID}"
+    unmount_on_exit "${LOCAL_FILESYSTEM_UUID}"
+fi
+
 restic --verbose check --read-data-subset 100M
