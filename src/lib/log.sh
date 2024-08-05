806d3c5f4dcf667e579c66439a550efd3959b386
Philip Crockett <contact@philcrockett.com>
2024-08-05 11:48:20 +0200

    clean up logging, stdlib, and color output

--- src/lib/log.sh
+++ src/lib/log.sh
@@ -5,17 +5,38 @@ log:info() {
 }
 
 log:success() {
-    green "${*}\n"
+    log:green "${*}\n"
 }
 
 log:error() {
-    red "${*}\n"
+    log:red "${*}\n"
 }
 
 log:step() {
-    cyan "--> ${*}\n"
+    log:cyan "--> ${*}\n"
 }
 
 log:indent() {
     sed 's/^/    /'
 }
+
+_output_is_tty() { [[ -t 1 ]]; }
+
+_print_formatted() {
+    local format="${1}"
+    shift
+    if _output_is_tty; then
+        printf "%b%b\e[0m" "${format}" "${*}"
+    else
+        printf "%b" "${*}"
+    fi
+}
+
+log:red() { _print_formatted "\e[31m" "${*}"; }
+log:green() { _print_formatted "\e[32m" "${*}"; }
+log:yellow() { _print_formatted "\e[33m" "${*}"; }
+log:blue() { _print_formatted "\e[34m" "${*}"; }
+log:magenta() { _print_formatted "\e[35m" "${*}"; }
+log:cyan() { _print_formatted "\e[36m" "${*}"; }
+log:bold() { _print_formatted "\e[1m" "${*}"; }
+log:underlined() { _print_formatted "\e[4m" "${*}"; }
--- src/lib/stdlib.sh
+++ src/lib/stdlib.sh
@@ -1,34 +1,7 @@
 # shellcheck shell=bash
 
-# I'm calling this my "standard library" -- no namespaces required for these functions
-
 panic() {
     # for when you really shouldn't keep calm and carry on
-    red "FATAL: ${*}\n" >&2
+    log:red "FATAL: ${*}\n" >&2
     exit 1
 }
-
-not_implemented() {
-    panic "Not implemented yet."
-}
-
-_output_is_tty() { [[ -t 1 ]]; }
-
-_print_formatted() {
-    local format="${1}"
-    shift
-    if _output_is_tty; then
-        printf "%b%b\e[0m" "${format}" "${*}"
-    else
-        printf "%b" "${*}"
-    fi
-}
-
-red() { _print_formatted "\e[31m" "${*}"; }
-green() { _print_formatted "\e[32m" "${*}"; }
-yellow() { _print_formatted "\e[33m" "${*}"; }
-blue() { _print_formatted "\e[34m" "${*}"; }
-magenta() { _print_formatted "\e[35m" "${*}"; }
-cyan() { _print_formatted "\e[36m" "${*}"; }
-bold() { _print_formatted "\e[1m" "${*}"; }
-underlined() { _print_formatted "\e[4m" "${*}"; }
