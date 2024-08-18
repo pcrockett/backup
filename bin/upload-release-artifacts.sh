da73590bf2f84fd806dd844ed0203378c8630571
Phil Crockett <contact@philcrockett.com>
2024-08-18 20:47:37 +0200

    fix(release): upload artifacts after release (#21)

--- bin/upload-release-artifacts.sh
+++ bin/upload-release-artifacts.sh
@@ -5,9 +5,28 @@ current_release() {
     grep --only-matching --perl-regexp '(?<="\.":\s")\d+\.\d+\.\d+(?=")' .release-please-manifest.json
 }
 
+asset_count() {
+    local count
+    if count="$(gh release view --json assets --jq '.assets | length' "v${1}")"; then
+        echo "${count}"
+    else
+        echo ""
+    fi
+}
+
 main() {
-    current_release
-    gh release upload --clobber "v$(current_release)" ./backup
+    local current_release_version current_release_asset_count
+    current_release_version="$(current_release)"
+    current_release_asset_count="$(asset_count "${current_release_version}")"
+
+    if [ "${current_release_asset_count}" == "" ]; then
+        echo "Release v${current_release_version} hasn't been created yet. Nothing to upload."
+    elif [ "${current_release_asset_count}" -eq 0 ]; then
+        make devenv-build
+        gh release upload "v${current_release_version}" ./backup
+    else
+        echo "Release v${current_release_version} already has assets. No need to upload anything new."
+    fi
 }
 
 main
