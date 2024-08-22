b23d8965cd02125007570786a5ff48983b27138f
Philip Crockett <contact@philcrockett.com>
2024-08-22 20:39:17 +0200

    refactor: remove compose, simplify docker / devenv

--- tests/init-test-bucket.sh
+++ tests/init-test-bucket.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-THIS_SCRIPT="$(readlink -f "${0}")"
-
 init() {
-    TESTS_DIR="$(dirname "${THIS_SCRIPT}")"
-    REPO_DIR="$(dirname "${TESTS_DIR}")"
-    COMPOSE_FILE="${BACKUP_COMPOSE_FILE:-${REPO_DIR}/compose.yml}"
     MINIO_INSTANCE_NAME="${MINIO_INSTANCE_NAME:-testinstance}"
     MINIO_INSTANCE_URL="${MINIO_INSTANCE_URL:-http://localhost:9000}"
     MINIO_ROOT_USER="${MINIO_ROOT_USER:-adminuser}"
@@ -17,76 +12,54 @@ init() {
     MINIO_TEST_SECRET_KEY="${MINIO_TEST_SECRET_KEY:-testsecretkey}"
     MINIO_BUCKET_NAME="${MINIO_BUCKET_NAME:-testbucket}"
     MINIO_DATA_DIR="${MINIO_DATA_DIR:-/data}"
-}
 
-compose_cmd() {
-    docker compose --file "${COMPOSE_FILE}" "${@}"
-}
-
-minio_cmd() {
-    if command -v mc &> /dev/null; then
-        # we already have minio in our environment
-        # (ex: we are running inside a docker container already with mc installed)
-        # (used in CI)
-        pushd "${MINIO_DATA_DIR}" &> /dev/null
-        if "${@}"; then
-            result=0
-        else
-            result=1
-        fi
-        popd &> /dev/null
-        return ${result}
-    else
-        # we're running on a host somewhere that is running minio via docker compose
-        # (used during local development)
-        compose_cmd exec --workdir "${MINIO_DATA_DIR}" --no-TTY minio "${@}"
-    fi
+    cd "${MINIO_DATA_DIR}"
 }
 
 setup_alias() {
-    if minio_cmd mc alias list "${MINIO_INSTANCE_NAME}" &> /dev/null; then
+    if mc alias list "${MINIO_INSTANCE_NAME}" &> /dev/null; then
         return 0
     else
-        minio_cmd mc alias set \
+        mc alias set \
             "${MINIO_INSTANCE_NAME}" "${MINIO_INSTANCE_URL}" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}" \
             > /dev/null
     fi
 }
 
 setup_user() {
-    if minio_cmd mc admin user info "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" &> /dev/null; then
+    if mc admin user info "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" &> /dev/null; then
         return 0
     else
-        minio_cmd mc admin user add "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" "${MINIO_TEST_PASSWORD}"
+        mc admin user add "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" "${MINIO_TEST_PASSWORD}"
     fi
 }
 
 setup_accesskey() {
-    if minio_cmd mc admin user svcacct info "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_ACCESS_KEY}" &> /dev/null; then
+    if mc admin user svcacct info "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_ACCESS_KEY}" &> /dev/null; then
         return 0
     else
-        minio_cmd mc admin user svcacct add "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" \
+        mc admin user svcacct add "${MINIO_INSTANCE_NAME}" "${MINIO_TEST_USER}" \
             --access-key "${MINIO_TEST_ACCESS_KEY}" \
             --secret-key "${MINIO_TEST_SECRET_KEY}"
     fi
 }
 
 setup_policy() {
-    if minio_cmd mc admin policy entities "${MINIO_INSTANCE_NAME}" --user "${MINIO_TEST_USER}" | grep readwrite &> /dev/null; then
+    if mc admin policy entities "${MINIO_INSTANCE_NAME}" --user "${MINIO_TEST_USER}" | grep readwrite &> /dev/null; then
         return 0
     else
-        minio_cmd mc admin policy attach "${MINIO_INSTANCE_NAME}" readwrite --user "${MINIO_TEST_USER}"
+        mc admin policy attach "${MINIO_INSTANCE_NAME}" readwrite --user "${MINIO_TEST_USER}"
     fi
 }
 
 remove_bucket() {
     # removes the bucket if it exists, and does nothing otherwise
-    minio_cmd test ! -d "${MINIO_BUCKET_NAME}" \
-        || minio_cmd mc rb --force "${MINIO_INSTANCE_NAME}/${MINIO_BUCKET_NAME}"
+    test ! -d "${MINIO_BUCKET_NAME}" \
+        || mc rb --force "${MINIO_INSTANCE_NAME}/${MINIO_BUCKET_NAME}"
 }
 
 create_bucket() {
-    minio_cmd mc mb "${MINIO_INSTANCE_NAME}/${MINIO_BUCKET_NAME}"
+    mc mb "${MINIO_INSTANCE_NAME}/${MINIO_BUCKET_NAME}"
 }
 
 main() {
--- tests/minio-stop.sh
+++ tests/minio-stop.sh
@@ -6,15 +6,7 @@ STATE_DIR="${XDG_STATE_HOME:-"${HOME}/.local/state"}/backup"
 PID_FILE="${STATE_DIR}/minio-pid"
 mkdir --parent "${STATE_DIR}"
 
-if command -v mc &> /dev/null; then
-    # we already have minio in our environment
-    # (ex: we are running inside a docker container already with mc installed)
-    if [ -f "${PID_FILE}" ]; then
-        kill "$(head -n 1 "${PID_FILE}")" || true
-        rm "${PID_FILE}"
-    fi
-else
-    # we're running on a host somewhere that is running minio via docker compose
-    # (used during local development)
-    docker compose down
+if [ -f "${PID_FILE}" ]; then
+    kill "$(head -n 1 "${PID_FILE}")" || true
+    rm "${PID_FILE}"
 fi
