95b3fd690830a84808fa78fc3583dd6cf28c79e6
Philip Crockett <contact@philcrockett.com>
2024-09-14 22:10:28 +0200

    wip

--- src/automagic_command.sh
+++ src/automagic_command.sh
@@ -4,12 +4,16 @@ test -f "$(config:file_path)" || panic "No backup has been configured yet for th
 
 THIS_SCRIPT_PATH="$(readlink -f "${0}")"
 AUTOMAGIC_SCRIPT_PATH="$(config:directory_path)/automagic.sh"
+AUTOMAGIC_HOOKS_DIR="$(config:directory_path)/automagic_hooks"
+BEFORE_HOOK_SCRIPT_PATH="${AUTOMAGIC_HOOKS_DIR}/before.sh"
+AFTER_HOOK_SCRIPT_PATH="${AUTOMAGIC_HOOKS_DIR}/after.sh"
 SYSTEMD_UNIT_NAME="automagical-backup"
 SYSTEMD_UNIT_PATH="/etc/systemd/system/${SYSTEMD_UNIT_NAME}.service"
 
 if [ "${args[--uninstall]:-}" != "" ]; then
     systemctl disable --now "${SYSTEMD_UNIT_NAME}.service"
     rm -f "${AUTOMAGIC_SCRIPT_PATH}" "${SYSTEMD_UNIT_PATH}"
+    rm -rf "${AUTOMAGIC_HOOKS_DIR}"
     systemctl daemon-reload
     exit 0
 fi
@@ -19,11 +23,48 @@ config:read
 AUTOMAGIC_SCRIPT="$(cat <<EOF
 #!/usr/bin/env bash
 set -Eeuo pipefail
+
 BACKUP_SCRIPT=$(util:escape_value "${THIS_SCRIPT_PATH}")
 export HOME=/root  # Restic wants to know where HOME is for caching purposes
-"\${BACKUP_SCRIPT}" run
-"\${BACKUP_SCRIPT}" check
-# TODO: run / check different destinations in parallel as background jobs?
+
+run_and_check() {
+    "\${BACKUP_SCRIPT}" run
+    "\${BACKUP_SCRIPT}" check
+}
+
+"${BEFORE_HOOK_SCRIPT_PATH}"
+
+if run_and_check; then
+    export AUTOMAGIC_BACKUP_RESULT=0
+else
+    export AUTOMAGIC_BACKUP_RESULT=${?}
+fi
+
+"${AFTER_HOOK_SCRIPT_PATH}" || true
+
+exit \${AUTOMAGIC_BACKUP_RESULT}
+EOF
+)"
+
+AFTER_HOOK_SCRIPT="$(cat <<EOF
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Script inputs:
+#
+# * AUTOMAGIC_BACKUP_RESULT environment variable indicating exit code of EITHER the \`run\` or
+#   \`check\` phase.
+#
+EOF
+)"
+
+
+BEFORE_HOOK_SCRIPT="$(cat <<EOF
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# TODO
+
 EOF
 )"
 
@@ -45,15 +86,23 @@ EOF
 )"
 
 (
-    umask u=rw,g=r,o=r
+    umask u=rw,g=,o=
 
-    echo "${SYSTEMD_UNIT}" > "${SYSTEMD_UNIT_PATH}"
-    log:info "Created ${SYSTEMD_UNIT_PATH}"
-
+    mkdir --parent "${AUTOMAGIC_HOOKS_DIR}"
+    echo "${AFTER_HOOK_SCRIPT}" > "${AFTER_HOOK_SCRIPT_PATH}"
+    echo "${BEFORE_HOOK_SCRIPT}" > "${BEFORE_HOOK_SCRIPT_PATH}"
     echo "${AUTOMAGIC_SCRIPT}" > "${AUTOMAGIC_SCRIPT_PATH}"
     log:info "Created ${AUTOMAGIC_SCRIPT_PATH}"
 )
 
-chmod +x "${AUTOMAGIC_SCRIPT_PATH}"
+chmod +x "${AUTOMAGIC_SCRIPT_PATH}" "${BEFORE_HOOK_SCRIPT_PATH}" "${AFTER_HOOK_SCRIPT_PATH}"
+
+(
+    umask u=rw,g=r,o=r
+
+    echo "${SYSTEMD_UNIT}" > "${SYSTEMD_UNIT_PATH}"
+    log:info "Created ${SYSTEMD_UNIT_PATH}"
+)
+
 systemctl daemon-reload
 systemctl enable "${SYSTEMD_UNIT_NAME}.service"
