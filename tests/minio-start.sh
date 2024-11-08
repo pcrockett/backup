fa537418da6bc24b6c1a1114cc38e2147a51726c
Phil Crockett <contact@philcrockett.com>
2024-11-08 21:28:23 +0100

    fix: minio start race condition on slower devices

--- tests/minio-start.sh
+++ tests/minio-start.sh
@@ -14,3 +14,9 @@ fi
 minio server "${MINIO_DATA_DIR}" &> "${LOG_FILE}" &
 echo "${!}" > "${PID_FILE}"
 echo "Minio logs are being stored in ${LOG_FILE}"
+
+# HACK: on my slow laptop, the first test fails because it starts
+# running before minio is all the way up. i could fix this the _right_
+# way by waiting for an http endpoint to respond... or i could fix
+# it the lazy way that'll probably be good enough:
+sleep 1
